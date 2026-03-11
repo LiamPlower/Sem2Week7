172a67b777b19b789c3ff9ca76ca7f991577487a
LiamPlower-STUDENT <G00467316@atu.ie>
2026-03-11 13:57:23 +0000

    Created project

--- src/main/java/ie/atu/week7solution/model/ReservationModel.java
+++ src/main/java/ie/atu/week7solution/model/ReservationModel.java
@@ -1,4 +1,37 @@
 package ie.atu.week7solution.model;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class ReservationModel {
+
+    private Long reservationId;
+
+    @NotBlank(message = "equipmentTag is required")
+    private String equipmentTag;
+
+    @NotBlank(message = " studentEmail is required")
+    @Email(message = " studentEmail must be a valid email address")
+    private String studentEmail;
+
+    @NotNull(message = "reservation is required")
+    private LocalDate reservationDate;
+
+    @Min(value = 0, message = "startHour must be between 0 and 23")
+    @Max(value = 23, message = "startHour must be between 0 and 23")
+    private int startHour;
+
+    @Min(value = 1, message = "durationHours must be between 1 and 24")
+    @Max(value = 24, message = "durationHours must be between 1 and 24")
+
+
 }
